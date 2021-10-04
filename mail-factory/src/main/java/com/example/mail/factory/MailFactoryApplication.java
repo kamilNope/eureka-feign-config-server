c6ec50b5e0492ecaacb358aef718a77dc6caaa24
Kamil Niedzielak <kamil.niedzielak.ctr@sabre.com>
2021-10-04 23:03:16 +0200

    init eureka server

---
+++ mail-factory/src/main/java/com/example/mail/factory/MailFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.mail.factory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailFactoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailFactoryApplication.class, args);
+    }
+
+}
---
+++ mail-factory/src/test/java/com/example/mail/factory/MailFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mail.factory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailFactoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
